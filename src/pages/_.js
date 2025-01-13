473df58efbc864253cf4a28b4f4af32ce4d38c87
psy1218 <141633727+psy1218@users.noreply.github.com>
2025-01-13 23:37:41 +0900

    네비게이트 완료료

--- src/pages/_.js
+++ src/pages/_.js
@@ -82,13 +82,7 @@ const _2 = () => {
                 <a href="/My_board" className={styles["menu-item"]}>작성 게시글</a>
                 <a href="/My_message" className={styles["menu-item"]}>작성 댓글</a>
                 <a href="/add" className={styles["menu-item"]}>스터디 신청 확인</a>
-              <a 
-                href="#" 
-                onClick={handleLogoutClick} 
-                className={`${styles["menu-item"]} ${styles["logout"]}`} 
-              >
-                로그아웃
-              </a>
+             
             </div>
           )}
 
