1eb28e376c5786996bfbcafb0c6978a258a1fe0d
석진 김 <kkbksj00@gmail.com>
2025-01-15 23:40:19 +0900

    글쓰기 css 최종 수정

--- src/pages/G_HomePage.js
+++ src/pages/G_HomePage.js
@@ -25,7 +25,7 @@ import { useMediaQuery } from 'react-responsive'; // 반응형 페이지 만들�
 
 
 import axios from 'axios';
-import jwtDecode from 'jwt-decode';
+import {jwtDecode} from 'jwt-decode';
 
 
 const BASE_URL = 'http://info-rmation.kro.kr';
@@ -37,20 +37,20 @@ const BASE_URL = 'http://info-rmation.kro.kr';
 
 // 인증 헤더 가져오기 함수
 const getAuthHeaders = () => {
-  const accessToken = localStorage.getItem('authToken');
+  const authToken = localStorage.getItem('authToken');
 
-  if (!accessToken) {
-    console.warn('Access token is missing');
+  if (!authToken) {
+    console.warn('auth token is missing');
     return {};
   }
 
   try {
-    const decodedToken = jwtDecode(accessToken);
+    const decodedToken = jwtDecode(authToken);
     const userId = decodedToken?.userId || '';
     console.log('Decoded Token:', decodedToken);
 
     return {
-      Authorization: `Bearer ${accessToken}`,
+      Authorization: `Bearer ${authToken}`,
       'X-USER-ID': userId,
     };
   } catch (error) {
@@ -222,10 +222,10 @@ const G_HomePage = () => {
   const handleRoomClick = (path) => {
     navigate(`/${path}`);  // 방 ID에 맞는 페이지로 이동
   };
-  //   const accessToken = localStorage.getItem('accessToken');
-  // if (!accessToken) throw new Error('사용자 인증 정보가 없습니다.');
+  //   const authToken = localStorage.getItem('authToken');
+  // if (!authToken) throw new Error('사용자 인증 정보가 없습니다.');
 
-  //const decodedToken = jwtDecode(accessToken);
+  //const decodedToken = jwtDecode(authToken);
   //const userId = decodedToken.userId;
 
 
